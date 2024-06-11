5e734516aa7b361b7780c0956d111b654227f749
Tiffca17 <92198529+Tiffca17@users.noreply.github.com>
2024-06-10 21:58:05 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -25,7 +25,9 @@ format = "%Y-%m-%d %H:%M:%S %Z%z"
 
 origins = ["http://127.0.0.1:8000", 
            "https://simple-smart-hub-client.netlify.app",
-           "http://192.168.102.46:8000"]
+           "http://192.168.102.46:8000",
+          "http://127.0.0.1:5500/index.html",
+          "https://test-8gc8.onrender.com"]
 
 app.add_middleware(
     CORSMiddleware,
